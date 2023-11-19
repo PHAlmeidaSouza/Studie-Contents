d4f537e6ef06cb9ee2a3604fd3ede1b3a8661792
Pedro Henrique <ph8584774@gmail.com>
2023-11-19 19:46:27 -0300

    Testes com os métodos estáticos do JUnit com exceções

--- DSlearn/aula-junit/src/tests/entities/AccountTests.java
+++ DSlearn/aula-junit/src/tests/entities/AccountTests.java
@@ -3,17 +3,57 @@ package tests.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
     @Test
-    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
-         double amount = 200.0;
-         double expectedValue = 196.0;
-         Account acc = new Account(1L, 0.0);
+    public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
+         Double amount = 200.0;
+         Double expectedValue = 196.0;
+         Account acc = AccountFactory.createEmptyAccount();
 
          acc.deposit(amount);
 
          Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount() {
+        Double expectedValue = 100.0;
+        Account acc = AccountFactory.createAccount(expectedValue);
+        Double amount = -200.0;
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+        double expectedValue = 0.0;
+        double initialBalance = 800.0;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        Double result = acc.fullWithDraw();
+
+        Assertions.assertTrue(expectedValue == acc.getBalance());
+        Assertions.assertTrue(result == initialBalance);
+    }
+
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficentBalance() {
+        Account acc = AccountFactory.createAccount(800.0);
+        acc.withDraw(500.0);
+
+        Assertions.assertEquals(300.0, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Account acc = AccountFactory.createAccount(800.0);
+            acc.withDraw(800.1);
+        } );
     }
 }
