68a04ca1d23fbc962a1f530ea0d7198bb699798f
Pedro Henrique <ph8584774@gmail.com>
2023-11-20 21:05:35 -0300

    Finalização do exercicío de teste unitário utilizando JUnit

--- DSlearn/exercicio-testes-java/src/tests/entities/FinancingTests.java
+++ DSlearn/exercicio-testes-java/src/tests/entities/FinancingTests.java
@@ -24,7 +24,6 @@ public class FinancingTests {
 
     @Test
     public void shouldThrowIllegalArgumentExceptionWhenDataIsInvalid() {
-
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 20);
         });
@@ -37,5 +36,61 @@ public class FinancingTests {
         financing.setTotalAmount(90000.0);
 
         assertEquals(90000.0, financing.getTotalAmount());
+    }
+
+    @Test
+    public void setTotalAmountShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+            financing.setTotalAmount(110000.0);
+        });
+    }
+
+    @Test
+    public void setIncomeShouldSetDataWhenValidData() {
+        Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+
+        financing.setIncome(2100.0);
+
+        assertEquals(2100.0, financing.getIncome());
+    }
+
+    @Test
+    public void setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+            financing.setIncome(1900.0);
+        });
+    }
+
+    @Test
+    public void setMonthsShouldSetDataWhenValidData() {
+        Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+
+        financing.setMonths(90);
+
+        assertEquals(90, financing.getMonths());
+    }
+
+    @Test
+    public void setMonthsShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+            financing.setMonths(50);
+        });
+    }
+
+    @Test
+    public void shouldCalculateEntryValueCorrectly() {
+        Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+
+        assertEquals(20000.0, financing.entry());
+    }
+
+    @Test
+    public void shouldCalculateQuotaValueCorrectly() {
+        Financing financing = FinancingFactory.createFinancing(100000.0, 2000.0, 80);
+
+        assertEquals(1000.0, financing.quota());
     }
 }
