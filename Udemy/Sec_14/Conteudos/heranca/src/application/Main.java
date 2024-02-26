6e7cd38fa4a0b33b2b2339d1dfafcddc48005b95
Pedro Henrique <ph8584774@gmail.com>
2024-02-26 20:29:04 -0300

    Introducao a herança e UP/Down Casting

---
+++ Udemy/Sec_14/Conteudos/heranca/src/application/Main.java
@@ -0,0 +1,43 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Main {
+    public static void main(String[] args) {
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.0);
+
+        //BusinessAccount acc5 = (BusinessAccount) acc3;
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+
+    }
+}
+
+/*
+    Suponha um negócio de banco que possui uma conta comum e uma conta para
+    empresas, sendo que a conta para empresa possui todos membros da conta
+    comum, mais um limite de empréstimo e uma operação de realizar empréstimo.
+ */
