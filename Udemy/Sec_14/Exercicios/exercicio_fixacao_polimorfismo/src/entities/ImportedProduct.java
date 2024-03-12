728915cbc254b1a18e77b9a66ea7c444a030242f
Pedro Henrique <ph8584774@gmail.com>
2024-03-11 21:40:01 -0300

    Exercicio de fixacao - polimorfismo

---
+++ Udemy/Sec_14/Exercicios/exercicio_fixacao_polimorfismo/src/entities/ImportedProduct.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class ImportedProduct extends Product{
+    private Double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPrice() {
+        return getPrice() + customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return name + " $ "
+                + String.format("%.2f", price)
+                + " (Customs fee: $ "
+                + String.format("%.2f", customsFee)
+                + ")";
+    }
+
+}
