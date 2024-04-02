e47ecec77bea556b97e10d7b17503b85f52efc00
Pedro Henrique <pedro.henrique@itpower.co>
2024-04-02 11:54:03 -0300

    FileWriter e BufferedWriter

---
+++ Udemy/Sec_17/Conteudos/filewriter_and_bufferedwriter/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] lines = new String[]{"Good Morning", "Good Afternoon", "Good Night"};
+        String path = "C:\\Ws-Java\\Studie-Contents\\Udemy\\Sec_17\\Conteudos\\text1.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
